8ee0f081b02e71c71c049f3b26ea8f4277ec9e40
desktopGit <emilyxu@Emilys-MBP-2.attlocal.net>
2019-04-24 21:53:09 -0700

    toolbar added
    toolbars have back button and navigation drawer added

--- app/src/main/java/com/example/travelbuddy/Login.java
+++ app/src/main/java/com/example/travelbuddy/Login.java
@@ -1,18 +1,20 @@
 package com.example.travelbuddy;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 public class Login extends AppCompatActivity {
 
     Button login_button;
-    Button back_button;
     TextView username_input;
     TextView password_input;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +22,6 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.login);
 
         login_button = findViewById(R.id.button_login);
-        back_button = findViewById(R.id.button_back);
         username_input = findViewById(R.id.input_username);
         password_input = findViewById(R.id.input_password);
 
@@ -35,13 +36,19 @@ public class Login extends AppCompatActivity {
             }
         });
 
-        // clicking 'back' returns you to Login Activity
-        back_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Login.this, LoginActivity.class);
-                Login.this.startActivity(intent);
-            }
-        });
+        // back button on toolbar
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.arrow);
+        toolbar.setNavigationOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(Login.this, LoginActivity.class);
+                        Login.this.startActivity(intent);
+                    }
+                }
+        );
+
     }
 }
