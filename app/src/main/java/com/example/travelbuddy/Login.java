b77edcc4d3c9d8792675ce19429016ae4bde5cbd
desktopGit <emilyxu@Emilys-MBP-2.attlocal.net>
2019-04-24 18:52:45 -0700

    added 'login' activity
    linked 'login' button on LoginActivity to a new activity Login that takes in credentials and takes user to MainActivity

---
+++ app/src/main/java/com/example/travelbuddy/Login.java
@@ -0,0 +1,47 @@
+package com.example.travelbuddy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    Button login_button;
+    Button back_button;
+    TextView username_input;
+    TextView password_input;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        login_button = findViewById(R.id.button_login);
+        back_button = findViewById(R.id.button_back);
+        username_input = findViewById(R.id.input_username);
+        password_input = findViewById(R.id.input_password);
+
+        // clicking 'login' takes you to Main Activity
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username_string = username_input.getText().toString();
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                intent.putExtra("username", username_string);
+                Login.this.startActivity(intent);
+            }
+        });
+
+        // clicking 'back' returns you to Login Activity
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, LoginActivity.class);
+                Login.this.startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/travelbuddy/LoginActivity.java
+++ app/src/main/java/com/example/travelbuddy/LoginActivity.java
@@ -1,13 +1,28 @@
 package com.example.travelbuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+
+    Button login_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        // clicking 'log in' button takes you to Login activity
+        login_button = findViewById(R.id.login);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, Login.class);
+                LoginActivity.this.startActivity(intent);
+            }
+        });
     }
 }
