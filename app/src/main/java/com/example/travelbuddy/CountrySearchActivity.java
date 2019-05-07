b1d40da6b8fa171f01008ccfd74ee3633302c55f
Eliot Hsu <eliothsu@berkeley.edu>
2019-05-06 23:52:47 -0700

    Making search bar clickable and editable, correctly routing searches out to database

--- app/src/main/java/com/example/travelbuddy/CountrySearchActivity.java
+++ app/src/main/java/com/example/travelbuddy/CountrySearchActivity.java
@@ -2,15 +2,32 @@ package com.example.travelbuddy;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 public class CountrySearchActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "CountrySearch";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +46,48 @@ public class CountrySearchActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference myRef = database.getReference("countries");
+
+        // Get the SearchView and set the searchable configuration
+        SearchView searchView = (SearchView) findViewById(R.id.country_searchview);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.v(LOG_TAG, "search query updated: " + newText);
+                Query queryRef = myRef.orderByKey()
+                        .startAt("")
+                        .endAt("" + "\uf8ff");
+                ValueEventListener queryListener = new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Log.d(LOG_TAG, "search query RECEIVED DATA FROM FIREBASE REALTIME DATABASE: " + dataSnapshot.getValue().toString());
+                        populateResults((Map<String, Object>) dataSnapshot.getValue());
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                };
+                queryRef.addListenerForSingleValueEvent(queryListener);
+
+                return false;
+            }
+        });
+    }
+
+    public void populateResults(Map<String, Object> data) {
+        for (Map.Entry<String, Object> entry : data.entrySet()){
+            Map singleCountry = (Map) entry.getValue();
+            Log.d(LOG_TAG, "search query desc for " + entry.getKey() + " is " + singleCountry.get("desc"));
+        }
     }
 
     // create an action bar button
