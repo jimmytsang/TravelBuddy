5e4ebdcc3236186873c75096647407ad20d0a816
Eliot Hsu <eliothsu@berkeley.edu>
2019-05-07 19:06:17 -0700

    Formatting tips page

--- app/src/main/java/com/example/travelbuddy/TipsFragment.java
+++ app/src/main/java/com/example/travelbuddy/TipsFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
