334729bdf0589a40d70321507c5695dfd32fa9d8
Eliot Hsu <eliothsu@berkeley.edu>
2019-05-07 18:54:06 -0700

    Setting up placeholder items on the tips fragment

--- app/src/main/java/com/example/travelbuddy/TipsFragment.java
+++ app/src/main/java/com/example/travelbuddy/TipsFragment.java
@@ -1,5 +1,7 @@
 package com.example.travelbuddy;
 
+import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +9,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TipsFragment extends Fragment {
+
+    ExpandableListView expandableListView;
+    ExpandableListAdapter expandableListAdapter;
+    List<String> expandableListTitle;
+    HashMap<String, List<String>> expandableListDetail;
 
     public TipsFragment() {
         // Required empty public constructor
@@ -23,6 +39,168 @@ public class TipsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tips, container, false);
+        View rv = inflater.inflate(R.layout.fragment_tips, container, false);
+
+        expandableListView = rv.findViewById(R.id.expandableListView);
+        expandableListDetail = TipsFragmentData.getData();
+        expandableListTitle = new ArrayList(expandableListDetail.keySet());
+        expandableListAdapter = new ExpandableListViewAdapter(this.getContext(), expandableListTitle, expandableListDetail);
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                return false;
+            }
+        });
+
+        // ExpandableListView Group expanded listener
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+            }
+        });
+
+        // ExpandableListView Group collapsed listener
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+
+            }
+        });
+
+        expandableListView.setAdapter(expandableListAdapter);
+
+        return rv;
+    }
+}
+
+class TipsFragmentData {
+    public static HashMap getData() {
+        HashMap<String, List<String>> expandableListDetail = new HashMap<>();
+
+        List<String> technology = new ArrayList<>();
+        technology.add("Beats sued for noise-cancelling tech");
+        technology.add("Wikipedia blocks US Congress edits");
+        technology.add("Google quizzed over deleted links");
+        technology.add("Nasa seeks aid with Earth-Mars links");
+        technology.add("The Good, the Bad and the Ugly");
+
+        List<String> entertainment = new ArrayList<>();
+        entertainment.add("Goldfinch novel set for big screen");
+        entertainment.add("Anderson stellar in Streetcar");
+        entertainment.add("Ronstadt receives White House honour");
+        entertainment.add("Toronto to open with The Judge");
+        entertainment.add("British dancer return from Russia");
+
+        List<String> science = new ArrayList<>();
+        science.add("Eggshell may act like sunblock");
+        science.add("Brain hub predicts negative events");
+        science.add("California hit by raging wildfires");
+        science.add("Rosetta's comet seen in close-up");
+        science.add("Secret of sandstone shapes revealed");
+
+        expandableListDetail.put("TECHNOLOGY NEWS", technology);
+        expandableListDetail.put("ENTERTAINMENT NEWS", entertainment);
+        expandableListDetail.put("SCIENCE & ENVIRONMENT NEWS", science);
+        return expandableListDetail;
+    }
+}
+
+class ExpandableListViewAdapter extends BaseExpandableListAdapter {
+
+    private Context context;
+
+    // group titles
+    private List<String> listDataGroup;
+
+    // child data
+    private HashMap<String, List<String>> listDataChild;
+
+    public ExpandableListViewAdapter(Context context, List<String> listDataGroup,
+                                     HashMap<String, List<String>> listChildData) {
+        this.context = context;
+        this.listDataGroup = listDataGroup;
+        this.listDataChild = listChildData;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosititon) {
+        return this.listDataChild.get(this.listDataGroup.get(groupPosition))
+                .get(childPosititon);
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, final int childPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+
+        final String childText = (String) getChild(groupPosition, childPosition);
+
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_item, null);
+        }
+
+        TextView textViewChild = convertView.findViewById(R.id.textViewChild);
+
+        textViewChild.setText(childText);
+        return convertView;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this.listDataChild.get(this.listDataGroup.get(groupPosition))
+                .size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this.listDataGroup.get(groupPosition);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return this.listDataGroup.size();
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        String headerTitle = (String) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_group, null);
+        }
+
+        TextView textViewGroup = convertView
+                .findViewById(R.id.textViewGroup);
+        textViewGroup.setTypeface(null, Typeface.BOLD);
+        textViewGroup.setText(headerTitle);
+
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
     }
 }
